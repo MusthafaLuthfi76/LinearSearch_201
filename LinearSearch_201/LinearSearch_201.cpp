76c9df431a3203a926ffbab2634b1349b2424205
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-14 10:40:06 +0700

    prosedure input

--- LinearSearch_201/LinearSearch_201.cpp
+++ LinearSearch_201/LinearSearch_201.cpp
@@ -7,7 +7,25 @@ int i;                                      //Index of array element
 
 Void input()
 {
+    while (true)
+    {
+        cout << "Enter the number of elements in the Array = ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
 
+    //Accept array elements
+    cout << "\n--------------------\n";
+    cout << " Enter array elements \n";
+    cout << "-------------------- \n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
 int main()
